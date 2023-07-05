8f514e83ca572d2bc119b5a88b70df6630738936
Mansi Monpara <137049722+Mansimonpara@users.noreply.github.com>
2023-07-05 13:09:35 +0530

    Add files via upload

---
+++ DAY_13/buttonval.js
@@ -0,0 +1,5 @@
+$("button").on("click" , function() {
+    var text = $(this).text();
+    $("input").val(text);
+
+})
---
+++ DAY_13/inputval.js
@@ -0,0 +1,6 @@
+$("input").on("keyup" , function() {
+    var value = $(this).val();
+    $("p").text(value);
+
+})
+.trigger("keyup");
